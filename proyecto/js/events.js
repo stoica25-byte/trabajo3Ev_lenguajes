cb697daf75740cb930c8bcd45877c720d32bf000
alex <stoica25@iespabloserrano.com>
2026-05-12 09:00:36 +0200

    eventos +ui

--- proyecto/js/events.js
+++ proyecto/js/events.js
@@ -1,1 +1,9 @@
 // Gestión de eventos
+document.addEventListener('DomContentLoaded',()=>{
+    Dom_Equipos(datosEquipos);
+
+    const botonesNav = document.querySelectorAll('#navegador-lateral button'); //busca botones dentro del nav//
+    const secciones
+
+
+});
--- proyecto/js/ui.js
+++ proyecto/js/ui.js
@@ -32,7 +32,7 @@ function Dom_Equipos(listaEquipos) {
 
 }
 
-// js/ui.js
+
 
 function Dom_Jugadores(listaJugadores) {
     const contenedor = document.getElementById('contenedor-jugadores');
@@ -54,5 +54,60 @@ function Dom_Jugadores(listaJugadores) {
     });
 }
 
+function Dom_Partidos(listaPartidos){
+    const elementos_partidos = document.getElementById('lista-partidos');
+    elementos_partidos.textContent = '';
+
+    listaPartidos.forEach(partido =>{
+        const tarjeta = document.createElement('article');
+        tarjeta.classList.add('tarjeta-partido');
+
+        const encabezado = document.createElement('h4');
+        encabezado.textContent = 'Jornada ' + partido.jornada + ' | Fecha: ' + partido.fecha;
+
+        const marcador = document.createElement('p');
+        marcador.textContent = partido.equipo_local + ' ' + partido.goles_local + ' - ' + partido.goles_visitante + ' ' + partido.equipo_visitante;
+
+        tarjeta.appendChild(encabezado);
+        tarjeta.appendChild(marcador);
+
+        elementos_partidos.appendChild(tarjeta);
+    });
+}
+
+function Dom_Estadisticas(listaEstadisticas){
+const elementos_estadisticas = document.getElementById('grafica-estadisticas');
+elementos_estadisticas.textContent = '';
+
+let maxGoles = 0; 
+    listaEstadisticas.forEach(jugador => {
+        if(jugador.goles_totales > maxGoles){
+            maxGoles= jugador.goles_totales;
+        };
+
+    });
+
+    listaEstadisticas.forEach(jugador => {
+        const tarjeta = document.createElement('article');
+        tarjeta.classList.add('tarjeta-estadistica');
+
+       const infoTexto = document.createElement('p');
+        infoTexto.textContent = jugador.jugador + ' (' + jugador.equipo + ') - ' + jugador.goles_totales + ' goles';
+
+        
+
+        const barraEstadisticas = document.createElement('meter');
+        barraEstadisticas.min = 0;
+        barraEstadisticas.max = maxGoles;
+        barraEstadisticas.value = jugador.goles_totales;
+
+        tarjeta.appendChild(infoTexto);
+        tarjeta.appendChild(barraEstadisticas);
+
+        elementos_estadisticas.appendChild(tarjeta);
+
+    })
+
+}
 
 
